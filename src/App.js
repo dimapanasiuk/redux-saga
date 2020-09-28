1f6a87ada619c949da2d6e36ffeba97ecfe34789
dimapanasiuk <dimapanasiuk@icloud.com>
2020-09-28 12:46:42 +0300

    feat: implemntation get data with help redux saga

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
+import createSagaMiddleware from "redux-saga";
 
 import Counter from "./components/Counter";
+import Home from "./components/Home";
 import { rootReducer } from "./redux";
+import mySaga from "./sagas";
 
-const store = createStore(rootReducer);
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+sagaMiddleware.run(mySaga);
 
 const App = () => {
   return (
@@ -13,6 +19,7 @@ const App = () => {
       <Provider store={store}>
         <h1>App</h1>
         <Counter />
+        <Home />
       </Provider>
     </>
   );
--- src/redux/index.js
+++ src/redux/index.js
@@ -1,6 +1,9 @@
 import { combineReducers } from "redux";
 import counterReducer from "./counter/reducer";
 
+import data from "./saga/reducers/data";
+
 export const rootReducer = combineReducers({
   counter: counterReducer,
+  data,
 });
